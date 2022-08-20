74d31192d0012515292d65220c1ff1556298a55c
PapiaMCode <papiamcode@gmail.com>
2022-08-20 19:04:50 +0600

    Adding HomeView

--- QuizAppNew/View/HomeView.swift
+++ QuizAppNew/View/HomeView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            // MARK: Title....
+            Text("Revel Station")
+                .font(.system(size: 38))
+                .fontWeight(.heavy)
+                .foregroundColor(.purple)
+                .padding(.top)
+            // MARK: SUb Title....
+            Text("Choose the way \nyou play !!!")
+                .font(.title2)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.black)
+                .padding(.top, 8)
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
