0dcdab505720e05aae775f236ea3b20cafe0df73
PapiaMCode <papiamcode@gmail.com>
2022-08-20 17:49:02 +0600

    Initial Commit

---
+++ QuizAppNew/QuizAppNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppNewApp.swift
+//  QuizAppNew
+//
+//  Created by Jannatun Nahar Papia on 20/8/22.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
