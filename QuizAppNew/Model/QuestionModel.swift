c5880229f441dd9c7d74d8b0e28a7e02374d2ffe
PapiaMCode <papiamcode@gmail.com>
2022-08-23 02:39:04 +0600

    Creating Model

--- QuizAppNew/Model/QuestionModel.swift
+++ QuizAppNew/Model/QuestionModel.swift
@@ -7,3 +7,26 @@
 
 import Foundation
 
+struct QuestionModel:Identifiable, Codable {
+    var id: String { UUID().uuidString }
+    var question : String = ""
+    var optionA : String  = ""
+    var optionB : String  = ""
+    var optionC : String  = ""
+    var optionD : String  = ""
+    var answer: String  = ""
+    var sl : Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case question = "qustion"
+        case optionA = "option1"
+        case optionB = "option2"
+        case optionC = "option3"
+        case optionD  = "option4"
+        case answer = "answer"
+    }
+    
+    //for checking
+    var isSubmitted = false
+    var completed = false
+}
